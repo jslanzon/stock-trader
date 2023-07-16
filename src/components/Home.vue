6f85f156b348fffc11a5bd676007600fdc56b9f5
jslanzon <jslanzon@gmail.com>
2023-07-16 14:03:00 -0300

    portfolio

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        <h1>Início</h1>        
+        <h1 class="display-3 font-weight-light mb-4">Negocie e Consulte sua Ações</h1>
+        <v-sheet :elevation="12" class="pa-2" color="primary">
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline font-weight-light white--text">Você pode Salvar & Carregar os Dados</span>
+        </v-sheet>
+        <v-sheet :elevation="12" class="pa-2 mt-5 success darken-1" >
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline font-weight-light white--text">Clique em Finalizar dia para Iniciar Novo Dia!</span>
+        </v-sheet>
+        <v-divider class="my-4"></v-divider>
+        <p><strong>Seu Saldo: </strong> {{ funds }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        }
+    }
 }
 </script>
 <style>
